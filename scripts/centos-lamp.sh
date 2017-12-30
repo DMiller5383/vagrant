87ea68f7922f4e885a1d94500a9a77739470e38c
Daniel Miller <daniel.miller5383@gmail.com>
2017-12-30 14:53:52 +0000

    first commint

---
+++ scripts/centos-lamp.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#update CentOS with any patches
+yum update -y --exclude=kernel
+
+#Tools
+yum install -y nano git unzip screen
+
+#Apache
+yum install -y httpd httpd-devel httpd-tools
+chkconfig --add httpd-devel
+chkconfig httpd on
+service httpd stop
+
+rm -rf /var/www/html
+ln -s ~/sites/vagrant-test /var/www/html
+
+service httpd start
+
+#php
+yum install -y php php-cli php-common php-devel php-mysql
+
+#MySQL
+MySql -y mysql myql-server mysql-devel
+chkconfig --add mysqld
+chkconfig mysqld on
+
+service mysqld start
+
+mysql -u root -e "SHOW DATABASES";
+
+#DOWNLOAD started content
+
+service httpd restart
