7bb4386bb07228a1cbbf67588cc7f6a2c42f4c64
Daniel Miller <daniel.miller5383@gmail.com>
2017-12-30 14:58:07 +0000

    updated LAMP script with Github files

--- scripts/centos-lamp.sh
+++ scripts/centos-lamp.sh
@@ -30,5 +30,7 @@ service mysqld start
 mysql -u root -e "SHOW DATABASES";
 
 #DOWNLOAD started content
-
+cd ~/sites/vagrant-test
+sudo -u vagrant wget -q https://raw.githubusercontent.com/DMiller5383/vagrant/master/files/index.html
+sudo -u vagrant wget -q https://raw.githubusercontent.com/DMiller5383/vagrant/master/files/info.php
 service httpd restart
